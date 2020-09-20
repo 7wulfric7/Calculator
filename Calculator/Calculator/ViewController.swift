77f1b6a7c3c139db05d675f9aca440a59d26898f
Deniz Adil <xdenizadil@gmail.com>
2020-09-21 00:24:54 +0200

    Taged and made some functions that print unreal results

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -18,49 +18,71 @@ enum Operation: String {
 
 class ViewController: UIViewController {
     @IBOutlet weak var resultPrint: UILabel!
-    
+    var previousNumber = 0.0
+    var screenNumber = 0.0
+    var result = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func onClick(_ sender: UIButton) {
-        let tag = sender.tag
-        resultPrint.text = "\(tag)"
+        resultPrint.text = resultPrint.text! + String(sender.tag)
+        previousNumber = Double(resultPrint.text!)!
+        screenNumber = Double(resultPrint.text!)!
     }
     
     @IBAction func devide(_ sender: UIButton) {
         calculateOperation(operation: .devide)
+        if sender.tag == 13 {
+        result = previousNumber / screenNumber
+//        result = Double(sender.tag / sender.tag)
+        }
     }
     
     @IBAction func multiply(_ sender: UIButton) {
         calculateOperation(operation: .multiply)
+        if sender.tag == 12 {
+        result = previousNumber * screenNumber
+//        result = Double(sender.tag / sender.tag)
+        }
     }
     
     @IBAction func substract(_ sender: UIButton) {
         calculateOperation(operation: .subtract)
+        if sender.tag == 11 {
+        result = previousNumber - screenNumber
+//        result = Double(sender.tag / sender.tag)
+        }
     }
     
     @IBAction func add(_ sender: UIButton) {
         calculateOperation(operation: .add)
+        if sender.tag == 10 {
+        result = previousNumber + screenNumber
+//        result = Double(sender.tag / sender.tag)
+        }
     }
     
     @IBAction func equal(_ sender: UIButton) {
         calculateOperation(operation: .equal)
+        if sender.tag == 14 {
+        resultPrint.text = "\(result)"
+        }
     }
     
     func calculateOperation(operation: Operation) {
         switch operation {
         case .add:
-            break
+            resultPrint.text = "+"
         case .multiply:
-            break
+            resultPrint.text = "*"
         case .subtract:
-            break
+            resultPrint.text = "-"
         case .devide:
-            break
+            resultPrint.text = "/"
         case .equal:
-            break
+            resultPrint.text = "="
         case .empty:
             break
         }
